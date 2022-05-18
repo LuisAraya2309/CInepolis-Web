3c726c352dd53c3d8340e7f91e7c4ad665f0e99d
Luis <luistec2309@estudiantec.cr>
2022-05-17 21:04:40 -0600

    Base Arquitecture Ready

--- server/index.js
+++ server/index.js
@@ -4,31 +4,15 @@ const app = express()
 const mongoose = require('mongoose')
 const UserModel = require("./models/Users")
 
-app.use(express.json())
+const cors = require('cors')
 
+app.use(express.json())
+app.use(cors())
 
 mongoose.connect(
     "mongodb+srv://sa:admin@clustec.xtrci.mongodb.net/CinepolisWebDatabase?retryWrites=true&w=majority"
     );
 
-app.get("/getUsers",(req,res) => {
-    UserModel.find({}, (err,result) =>{
-        if (err){
-            res.json(err)
-        } else {
-            res.json(result)
-        }
-    })
-})
-
-
-app.post("/createUser", async (req,res) => {
-    const user = req.body;
-    const newUser = new UserModel(user);
-    await newUser.save();
-
-    res.json(user)
-})
 
 
 app.listen(3001,()=>{
---
+++ server/routes/users.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+const UserModel = require('../models/Users')
+//Login page
+router.post("/",async(req,res)=>{
+
+
+})
+
+
+module.exports(router);
